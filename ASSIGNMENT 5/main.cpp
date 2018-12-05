a8bd22d8d0dcf698feb4014d67efbd24264e28db
Tyler <smitht9@vcu.edu>
2018-12-05 15:46:18 -0500

    Initial Commit

---
+++ ASSIGNMENT 5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASSIGNMENT 5
+//
+//  Created by Tyler Smith on 12/5/18.
+//  Copyright © 2018 Tyler Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
